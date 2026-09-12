5382f044c9d0b847fff9c9c24c58126d99c11b21
Kanishka-dabas <kanishkadabas64@gmail.com>
2026-09-12 18:13:40 +0530

    Fix: use API_BASE_URL env var for Docker inter-container communication

--- frontend/api_client.py
+++ frontend/api_client.py
@@ -4,7 +4,8 @@ import json
 from src.config import settings
 
 
-API_BASE_URL = "http://127.0.0.1:8000"
+import os
+API_BASE_URL = os.environ.get("API_BASE_URL", "http://127.0.0.1:8000")
 BEARER_TOKEN = settings.security.bearer_token.get_secret_value()
 
 
